1cdf20af033e1a44047bea3369de56ee556cfc40
deelu96 <deelakameegoda@gmail.com>
2020-01-25 10:56:31 +0530

    state console output

--- src/msc/concurrent/MyThread.java
+++ src/msc/concurrent/MyThread.java
@@ -9,8 +9,10 @@ public class MyThread extends Thread{
     public void run(){
         for (int i = 0; i < 10; i++) {
             System.out.println(Thread.currentThread().getName()+" : "+i+" : Run");
+            System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
             try {
                 Thread.sleep(100);
+                System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
--- src/msc/concurrent/ThreadDemo.java
+++ src/msc/concurrent/ThreadDemo.java
@@ -5,17 +5,23 @@ import java.util.Scanner;
 public class ThreadDemo {
     public static void main(String[] args) {
         Thread t1 = new MyThread("Thread_One");
+        System.out.println(t1.getName() + " : " + t1.getState());
         t1.start();
+        System.out.println(t1.getName() + " : " + t1.getState());
 
         Thread t2 = new MyThread("Thread_Two");
+        System.out.println(t2.getName() + " : " + t2.getState());
         t2.start();
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             System.out.println(Thread.currentThread().getName() + " : " + i + " : Main");
+            System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
             if (i == 5) {
                 Scanner sc = new Scanner(System.in);
                 int num = sc.nextInt();
             }
+            System.out.println(t1.getName() + " : " + t1.getState());
+            System.out.println(t2.getName() + " : " + t2.getState());
         }
     }
 }
