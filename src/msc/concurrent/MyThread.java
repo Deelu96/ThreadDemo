295d81ef14204c1c6457915d46a87e0edcc76f0d
deelu96 <deelakameegoda@gmail.com>
2020-01-25 11:06:06 +0530

    Priority

--- src/msc/concurrent/MyThread.java
+++ src/msc/concurrent/MyThread.java
@@ -1,21 +1,21 @@
 package msc.concurrent;
 
-public class MyThread extends Thread{
+public class MyThread extends Thread {
 
     public MyThread(String name) {
         super(name);
     }
 
-    public void run(){
-        for (int i = 0; i < 10; i++) {
-            System.out.println(Thread.currentThread().getName()+" : "+i+" : Run");
-            System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
-            try {
+    public void run() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println(Thread.currentThread().getName() + " : " + i + " Priority : " + Thread.currentThread().getPriority());
+//            System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
+            /*try {
                 Thread.sleep(100);
-                System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
+//                System.out.println(Thread.currentThread().getName() + " : " + Thread.currentThread().getState());
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }
+            }*/
         }
     }
 }
